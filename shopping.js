2e0b7a873a73b8b6c150030d24392f2c369446ef
Thomas Witherow <twitherow@bellsouth.net>
2021-05-30 11:50:13 -0500

    initial push

---
+++ shopping.js
@@ -0,0 +1,36 @@
+
+ const shoppingList = [
+     {
+         item: "Milk",
+         price: 4.99
+     },
+     {
+         item: "Eggs",
+         price: 1.99
+
+     },
+     {
+         item: "Pizza rolls",
+         price: 2.99
+     },  
+     {
+         item: "Fish",
+         price: 4.69
+     },  
+     {
+         item: "Oranges",
+         price: 2.99
+     },  
+     {
+         
+         item: "Sandwich buns",
+         price:  2.49
+     },  
+     {
+         
+         item: "condiments",
+         price: 2.49
+     }
+     ]
+
+     console.log(shoppingList)
